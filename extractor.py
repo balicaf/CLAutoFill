0bfec1149c000efc13299fc23c46a3bab2e9badc
balicaf <37401133+balicaf@users.noreply.github.com>
2019-01-21 10:52:33 +0100

    vbs
    use .vbs like feature in python in order to send keystroke!

--- extractor.py
+++ extractor.py
@@ -1,6 +1,20 @@
-import openpyxl as O 
+#pip install openpyxl
+#pip install Pillow
+#pip install pywin32
+import openpyxl as O
+import time
+import win32com.client as comclt
+oShell= comclt.Dispatch("WScript.Shell")
+#oShell.AppActivate("Notepad") # select another application
+#oShell.Run("Notepad")
+#oShell.AppActivate("Notepad") # select another application CANw32
+#WScript.Sleep 7000
+time.sleep(5)
+oShell.SendKeys ("1003")
+oShell.sendkeys ("{ENTER}")
+oShell.SendKeys("a") # send the keys you want
 
-checklist = 'MS-TCU-AIVC_200701201841003187-D4b-ID20_2018-12-13.xlsm'
+checklist = 'test.xlsm'
 
 ls_id=['Vehicle definition','AIVC Activation status', 'USB Definition', 'Ecall Zone', 'Car Variant', 'DataRecord', 'Vehicle Manufacturer Spare Part R', 'ConfigurationFileReferenceLink data',
  'VehicleManuECUHWNumber', 'System Supplier ECU SW', 'HWVariantID', 'TCU state','IMEI', 'CCID', 'Identificatio (IMSI)', 'eUICC EID','eUICC MSISDN', 'MQTT payload compression']
@@ -12,7 +26,7 @@ L = list(zip(ls_id, ls_pos))
 data=[]
 
 def core():
-	with open("DiagConsoleLog.txt", "r") as f:
+	with open("traceLog.txt", "r") as f:
 		l = []
 		for line in f : 
 			line = line.strip()
@@ -29,6 +43,7 @@ def core():
 
 def fill_checklist(path, data_ls): 
 	ww = O.load_workbook(filename=path)
+	#ww = O.load_workbook("D:\test.xlsm")
 	ws = ww.worksheets[2]
 	for elem in data_ls: 
 		ws[elem[1]] = elem[0]
@@ -36,3 +51,5 @@ def fill_checklist(path, data_ls):
 if __name__ == "__main__" : 
 	core()
 	fill_checklist(checklist, data)
+
+
